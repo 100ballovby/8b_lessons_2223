71c9d5a0535ff0137ebbaa4850cad1c64d7afc8d
demidraksin <greatraksin@icloud.com>
2022-09-02 13:24:29 +0300

    Initial commit

---
+++ 00_основы/02_приведение_типов.py
@@ -0,0 +1,8 @@
+# int(x) - приводит х в тип данных "целое число"
+n1 = int(input('Введи число 1: '))
+n2 = int(input('Введи число 2: '))
+print('Hello, world!')
+print(n1 + n2)
+print(n1 * n2)
+print(n1 // n2)
+print(n1 % n2)
